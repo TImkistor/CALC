b986ea4c8e8bbd6e23ea697ba515a37697a1b32b
Tim_Fort <79075750+TImkistor@users.noreply.github.com>
2021-05-18 10:32:36 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(207, 69)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(51, 51)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(264, 69)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(51, 51)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(321, 69)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(51, 51)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(207, 126)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(51, 51)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(264, 126)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(51, 51)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(321, 126)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(51, 51)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(207, 183)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(51, 51)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(264, 183)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(51, 51)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(321, 183)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(51, 51)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(264, 240)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(51, 51)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "0"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(321, 240)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(51, 51)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "="
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(207, 240)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(51, 51)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = ","
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(161, 69)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(33, 51)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "+"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(122, 69)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(33, 51)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "-"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(161, 126)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(33, 51)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = "*"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(122, 126)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(33, 51)
+        Me.Button16.TabIndex = 15
+        Me.Button16.Text = "/"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(413, 69)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(33, 51)
+        Me.Button17.TabIndex = 16
+        Me.Button17.Text = " | |"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(452, 69)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(33, 51)
+        Me.Button18.TabIndex = 17
+        Me.Button18.Text = "√"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(413, 126)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(33, 51)
+        Me.Button19.TabIndex = 18
+        Me.Button19.Text = "^"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Location = New System.Drawing.Point(122, 12)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(79, 28)
+        Me.Button20.TabIndex = 19
+        Me.Button20.Text = "Pi"
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Location = New System.Drawing.Point(378, 12)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(33, 51)
+        Me.Button21.TabIndex = 20
+        Me.Button21.Text = "C"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(207, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(165, 27)
+        Me.TextBox1.TabIndex = 21
+        '
+        'Button22
+        '
+        Me.Button22.Font = New System.Drawing.Font("Segoe UI", 7.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button22.Location = New System.Drawing.Point(452, 127)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(33, 51)
+        Me.Button22.TabIndex = 22
+        Me.Button22.Text = "+/-"
+        Me.Button22.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(534, 307)
+        Me.Controls.Add(Me.Button22)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button21)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button22 As Button
+End Class
---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,345 @@
+﻿Public Class Form1
+    Dim k As Double
+    Dim d As Integer
+    'd=1 - сложение
+    'd=2 - вычетание
+    'd=3 - сложение
+    'd=4 - вычетание
+    'd=5 - если степень
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If TextBox1.Text = "Нельзя делить на 0" Or TextBox1.Text = "" Then
+        Else
+            k = TextBox1.Text
+            k = Math.Abs(k)
+            TextBox1.Text = k
+        End If
+
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        TextBox1.Text = ""
+        k = 0
+        d = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "1"
+            Else
+                TextBox1.Text &= "1"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "1"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "2"
+            Else
+                TextBox1.Text &= "2"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "2"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "3"
+            Else
+                TextBox1.Text &= "3"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "3"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "4"
+            Else
+                TextBox1.Text &= "4"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "4"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "5"
+            Else
+                TextBox1.Text &= "5"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "5"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "6"
+            Else
+                TextBox1.Text &= "6"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "6"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "7"
+            Else
+                TextBox1.Text &= "7"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "7"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "8"
+            Else
+                TextBox1.Text &= "8"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "8"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Try
+            If TextBox1.Text = "Нельзя делить на 0" Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+                TextBox1.Text = "9"
+            Else
+                TextBox1.Text &= "9"
+            End If
+        Catch ex As Exception
+            If TextBox1.Text = "" Then
+                TextBox1.Text = "9"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If TextBox1.Text = "Нельзя делить на 0" Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Or TextBox1.Text = "тестерам тут не рады" Then
+            TextBox1.Text = "0"
+        ElseIf TextBox1.Text = "-" Then
+            TextBox1.Text = "0"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "0"
+        Else
+            TextBox1.Text &= "0"
+        End If
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If Not TextBox1.Text = "" And Not TextBox1.Text = "+" Then
+            Try
+                k = TextBox1.Text
+                TextBox1.Text = ""
+                d = 1
+            Catch ex As Exception
+
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Select Case d
+            Case = 1
+                'Если сложение
+
+                If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+                    TextBox1.Text = k + TextBox1.Text
+                End If
+            Case = 2
+                'Если вычетание
+                If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+                    TextBox1.Text = k - TextBox1.Text
+                End If
+            Case = 3
+                'Если умножение
+                If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+                    TextBox1.Text = k * TextBox1.Text
+                End If
+            Case = 4
+                'Если деление
+                Try
+                    If k = 0 Or (TextBox1.Text.Last() <> "," And Convert.ToDouble(TextBox1.Text) = 0) Then
+                        TextBox1.Text = "Нельзя делить на 0"
+                    Else
+                        If k = Math.PI Or TextBox1.Text = Math.PI.ToString().Replace(".", ",") Then
+                            TextBox1.Text = "1" '
+                        Else
+                            TextBox1.Text = k / TextBox1.Text
+                        End If
+                    End If
+                Catch ex As Exception
+
+                End Try
+
+
+
+            Case = 5
+                'Если степень 
+                If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+                    Dim answer As Double = k ^ TextBox1.Text
+                    If answer = Double.PositiveInfinity Or answer = Double.NegativeInfinity Then
+                        TextBox1.Text = "тестерам тут не рады"
+                    Else
+                        TextBox1.Text = answer
+                    End If
+                End If
+        End Select
+
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        If Not TextBox1.Text.Contains(",") And TextBox1.Text <> "" Then
+            TextBox1.Text &= ","
+        End If
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        If Not TextBox1.Text.Contains(Math.PI.ToString().Replace(".", ",")) Then
+            TextBox1.Text = Math.PI.ToString().Replace(".", ",")
+        End If
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+            Try
+                k = TextBox1.Text
+                TextBox1.Text = ""
+                d = 2
+            Catch ex As Exception
+
+            End Try
+
+        End If
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+            Try
+                k = TextBox1.Text
+                TextBox1.Text = ""
+                d = 3
+            Catch ex As Exception
+
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+            Try
+                k = TextBox1.Text
+                TextBox1.Text = ""
+                d = 4
+            Catch ex As Exception
+
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        If Not TextBox1.Text = "" And Not TextBox1.Text = "-" Then
+            Try
+                k = TextBox1.Text
+                TextBox1.Text = ""
+                d = 5
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+
+        If TextBox1.Text = "Нельзя делить на 0" Or TextBox1.Text = "тестерам тут не рады" Or TextBox1.Text = "" Then
+
+        Else
+            k = TextBox1.Text
+            If k < 1 Then
+                TextBox1.Text = "тестерам тут не рады"
+            Else
+                k = Math.Sqrt(k)
+                TextBox1.Text = k
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = ""
+        ElseIf TextBox1.Text = "-" Then
+            TextBox1.Text = ""
+
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "0"
+        Else
+            Try
+                k = TextBox1.Text
+                k = k * (-1)
+                TextBox1.Text = k
+            Catch ex As Exception
+
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "-0" Then
+            TextBox1.Text = "0"
+        End If
+    End Sub
+End Class
